7333685b92958c69bae996fcfb613e81aaeef2f5
ozzyb1986 <oisin1986@gmail.com>
2025-05-30 17:24:45 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,2 +1,10 @@
-export default function App() {
-  return <h1>Hello from Signal Tower!</h1>;
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Signal Tower is Live!</h1>
+      <p>This is your first deployed React site.</p>
+    </div>
+  );
+}
